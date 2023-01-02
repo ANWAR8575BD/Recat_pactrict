726f7844a2d23bb650283c164c8fcf70aa9a1360
ANWAR8575BD <anwar8575bd@gmail.com>
2023-01-02 21:58:39 +0600

    first commit

---
+++ my_app/src/banner/Banner.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../banner/style.css";
+
+function Banner(){
+    return <div>
+        <h1 className="txt">Anwar Hossain</h1>
+    </div>
+}
+
+export default Banner;
